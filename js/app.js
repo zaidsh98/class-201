ebdb6676d00c7d19360df44650a1c8362fd0ed9e
zaidsh98 <sha7rouri29@gmail.com>
2021-05-23 15:40:44 +0300

    creating web

---
+++ js/app.js
@@ -0,0 +1,13 @@
+let consType = prompt ('What type of consol you play on ?')
+console.log(consType)
+
+let kind = prompt ('what is your favorite kind of Games ?')
+console.log(kind)
+
+let favGame = prompt ('what is your favorite Game ?')
+console.log(favGame)
+
+let favChar = prompt ('Who is your favorite Gaming character ?')
+console.log(favChar)
+
+ alert (`you play on   ${consType}  and you like playing this kind of games  ${kind}  your favorite game is   ${favGame} and  ${favChar} is your favorite character`)
